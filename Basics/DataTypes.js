ab34802dbf126db63c1b1000ac8325695ec81948
Garima Chandra <garimachandra2002@gmail.com>
2023-09-02 12:13:13 +0000

    Data Types

---
+++ Basics/DataTypes.js
@@ -0,0 +1,19 @@
+let name = "Garima"
+let age = 18 
+let islogged = true
+
+//number 
+//bigint
+//string=>""
+//boolean =>true / false
+//null => standalone value (its an object)
+
+console.log(typeof null);
+
+//undefined => type not defined (datatype)
+
+console.log(typeof undefined);
+
+//symbol => unique values
+
+//object
