a509ec5b08506fb83e280a0b54a0092353b200ef
Garima Chandra <garimachandra2002@gmail.com>
2023-09-03 08:31:07 +0000

    Comparision of values and datatypes

---
+++ Basics/Comparision_datatypes.js
@@ -0,0 +1,29 @@
+//Basics arithematic comparisions
+//-----------------------------------
+
+// console.log(2==1);
+// console.log(2>1);
+// console.log(2<1);
+// console.log(2>=1);
+// console.log(2<=1);
+
+
+//Comparision of different datatypes 
+// -----------------------------------
+// gives unpredictable outputs
+//try avoiding comparision of different data types
+
+// console.log(null>0);
+// console.log(null<0);
+// console.log(null==0);
+// console.log(null>=0);
+
+// console.log(undefined>0);
+// console.log(undefined<0);
+// console.log(undefined==0);
+// console.log(undefined>=0);
+
+//strict check => === (value + data types are checked)
+
+console.log(2 == "2");  //true
+console.log(2 === "2");  //false
