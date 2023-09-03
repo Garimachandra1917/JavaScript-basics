ff9f56ecc815dcd00a704256eebe6baf748485cb
Garima Chandra <garimachandra2002@gmail.com>
2023-09-03 09:40:14 +0000

    conversion

--- Basics/Datatype_Conversion.js
+++ Basics/Datatype_Conversion.js
@@ -66,3 +66,37 @@ let str = 19
 let strNum = String(str)
 console.log(typeof strNum)
 console.log( strNum)
+
+// *********************** Operations ***********************
+
+let valuen = 3
+let negValue = -valuen
+// console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2/3);
+// console.log(2%3);
+
+let str1 = "hello"
+let str2 = " hitesh"
+
+let str3 = str1 + str2
+// console.log(str3);
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+
+// console.log( (3 + 4) * 5 % 3);
+
+// console.log(+true);
+// console.log(+"");
+
+
+let gameCounter = 100
+++gameCounter;
+console.log(gameCounter);
